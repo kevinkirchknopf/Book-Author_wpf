296580f265eefb177b67893b3fe64188d4e3fa6c
Kirchknopf Kevin <90893689+kevinkirchknopf@users.noreply.github.com>
2024-04-10 09:57:03 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Book_Author_wpf.Modells;
+using Book_Author_wpf.Services;
+using Book_Author_wpf.ViewModells;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Book_Author_wpf;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        ///<summary>
+        ///A PLUSZ SZOLGÁLTATÁSOK HOZZÁADÁS
+        ///</summary>A
+        Services = ConfigureServices();
+
+        this.InitializeComponent();
+    }
+
+
+    //EZEN A METÓDUSON KERESZTÜL LEHET ELKÉRNI A VIEWMODEL-leket
+    public new static App Current => (App)Application.Current;
+
+
+    public IServiceProvider Services
+    {
+        get;
+    }
+
+
+
+    private static IServiceProvider ConfigureServices()
+    {
+        ///<summary>
+        ///A SZOLGÁLTATÁSOK-KÉSZLET PÉLDÁNYOSÍTÁSA
+        ///SAJÁT SZOLGÁTATÁS HOZZÁADÁSA
+        ///A BŐVÍTETT KÉSZLET VISSZAADÁSA
+        /// </summary>
+
+        var services = new ServiceCollection();
+
+        services.AddDbContext<B_A_Context>();
+
+        //HA VALAMELYIK KONSTRUKTOR IDataService PÉLDÁNYT KÉR, AKKOR EGY DataService PÉLDÁNYT AD AZ "APP"
+        services.AddSingleton<IBookService, BookService>();
+        services.AddSingleton<IAuthorService, AuthorService>();
+        services.AddTransient<AuthorListViewModell>();
+
+
+
+        //HA VALAKI AZ APP-tól KÉR EGY UserViewModel PÉLDÁNYT AKKOR INNÉT AD EGYET!
+  
+
+        return services.BuildServiceProvider();
+    }
+
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Book_Author_wpf.Modells;
+using Book_Author_wpf.Views;
+
+namespace Book_Author_wpf;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    B_A_Context dbc;
+    private AuthorListView authorListView;
+
+    public MainWindow()
+    {
+        dbc = new B_A_Context();
+        dbc.Database.EnsureCreated();
+
+        dbc.SaveChanges();
+        InitializeComponent();
+        authorListView = new AuthorListView();
+        ccAblak = authorListView;
+
+
+
+      
+
+    }
+}
