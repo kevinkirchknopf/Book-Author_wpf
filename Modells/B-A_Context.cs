eff7407c1a4319f5832d2ed522dbed4f53e33d30
Kirchknopf Kevin <90893689+kevinkirchknopf@users.noreply.github.com>
2024-04-10 09:58:54 +0200

    Add files via upload

---
+++ Modells/B-A_Context.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Author_wpf.Modells;
+public class B_A_Context : DbContext
+{
+
+    public DbSet<Author> Author
+    {
+    get; set; 
+    }
+
+    public DbSet<Book> Books
+    {
+    get; set; }
+
+    public DbSet<Country> Country
+    {
+    get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //ConnectionString
+        string c = "server = localhost; userid = root; password =; database = Book_Author_WPF";
+
+        //OptionBulider: ConnectionString + Szerver verzió (manuálisan, vagy Autodetect)
+        optionsBuilder.UseMySql(c, ServerVersion.AutoDetect(c));
+
+
+    }
+
+}
